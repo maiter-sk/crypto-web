8be271ef528bb52c655c98ff02459520b7c1a966
maiter-sk <mattoff2012@gmail.com>
2026-09-10 20:09:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const priceHistory = {};
 for (let sym in CRYPTO_MARKET) { priceHistory[sym] = [CRYPTO_MARKET[sym].price]; }
 
 const SERVER_URL = 'https://crypto-sim1-1.onrender.com';
-const APP_VERSION = '20260910-21';
+const APP_VERSION = '20260910-22';
 const MINING_REWARD_EUR_PER_HOUR = 100;
 const RIG_COST_USD = 100;
 let currentUser = null;
